0f9906d6036175a022eaed29c534b1e093b35f9f
Ken <kennyk3n@live.com>
2021-01-16 14:52:58 -0500

    render template at root

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 import express from "express";
 import path from "path";
 import devBundle from "./devBundle"; // development mode only
+import template from "./../template";
 
 const app = express();
+devBundle.compile(app); // development mode only
+
 const CURRENT_WORKING_DIR = process.cwd();
 
 app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 
-devBundle.compile(app); // development mode only
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info(`Server started on port ${port}`);
+});
