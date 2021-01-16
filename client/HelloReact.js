7b845e4875611302ce47957e294d200778c94ae7
Ken <kennyk3n@live.com>
2021-01-16 13:59:39 -0500

    setup front end views

---
+++ client/HelloReact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const HelloReact = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+};
+
+export default hot(module)(HelloReact);
