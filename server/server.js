f987257dd4acce7f6d4279f38511a6143913a1bf
Ken <kennyk3n@live.com>
2021-01-16 14:47:32 -0500

    setup serving static files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 import express from "express";
+import path from "path";
 import devBundle from "./devBundle"; // development mode only
 
 const app = express();
+const CURRENT_WORKING_DIR = process.cwd();
+
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
 devBundle.compile(app); // development mode only
