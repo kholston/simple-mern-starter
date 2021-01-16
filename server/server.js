9f24f95e4b109fdc54f0ee4e3080e3b4fb925664
Ken <kennyk3n@live.com>
2021-01-16 15:01:31 -0500

    setup mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 import devBundle from "./devBundle"; // development mode only
 import template from "./../template";
 
@@ -13,6 +14,17 @@ app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 app.get("/", (req, res) => {
   res.status(200).send(template());
 });
+
+mongoose.connect(
+  process.env.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.info("Connected successfully to mongodb server");
+  }
+);
 
 let port = process.env.PORT || 3000;
 app.listen(port, function onStart(err) {
